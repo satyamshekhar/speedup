b4dfa9e6b0fa5655e6764c7433fcb35f22499613
Satyam Shekhar <satyamshekhar@gmail.com>
2011-08-03 12:59:31 +0530

    Adding linked-list

---
+++ src/exceptions.list.js
@@ -0,0 +1,18 @@
+exports = {
+    ElementNotFoundException: {
+        code: 1,
+        message: 'Element was not found'
+    },
+    OutOfBoundException: {
+        code: 2,
+        message: 'Index out of Bound'
+    },
+    NullPointerException: {
+        code: 3,
+        message: 'Tried to insert null value in structure'
+    },
+    NoSuchElementException: {
+        code: 4,
+        message: 'Tried to access element that is not there'
+    }
+};
