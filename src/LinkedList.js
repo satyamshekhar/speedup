a2a21aa827a508343c1a1e9ec928f88e0e63797f
Satyam Shekhar <satyamshekhar@gmail.com>
2012-10-28 05:35:38 -0700

    Reimplementing Linked List.

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,185 +1,142 @@
-var assert = require('assert').ok;
-var exceptions = require('exceptions');
+var assert = require("assert").ok;
 
-function LinkedList() {
+var LinkedList = module.exports = function LinkedList() {
     this._head = null;
     this._tail = null;
     this._size = 0;
-}
+};
 
-LinkedList.prototype = {
-    _new_node: function (obj) {
-        if (obj === null || obj === undefined) {
-            throw exception.NullPointerException;
-        }
-        return {
-            prev: null,
-            next: null,
-            obj: obj
-        };
-    },
+var Node = exports.Node = function Node(obj) {
+    this._prev = null;
+    this._next = null;
+    this.data = obj;
+};
 
-    _get_node: function (index) {
-        //-1 when finding the place for element 
-        //to be inserted at index 0
-        assert(index < this._size && index > -2);
-        
-        if (index < 0) {
-            return null;
-        }
-        
-        var node = this._head;
-        var i;
-        for (i = 0 ; i < index ; i++) {
-            node = node.next;
-        }
-        return node;
-    },
+// assume that typeof index is number.
+// if index is out of range, we insert at end.
+LinkedList.prototype.insert = function (obj, index) {
+    var node = this._createNodeIfNot(obj);
+    index = index === undefined ? this._size : index;
+    assert(index <= this._size && index >= 0);
 
-    get: function (index) {
-        if (index >= this._size || index < 0) {
-            throw exceptions.OutOfBoundException;
-        }
-        var node = this._get_node(index);
-        return node.obj;
-    },
-
-    getFirst: function () {
-        if (this._size < 1) {
-            throw exception.NoSuchElementException;
-        }
-        return this.get(0);
-    },
-    
-    getLast: function () {
-        if (this._size < 1) {
-            throw exception.NoSuchElementException;
-        }
-        this.get(this._size - 1);
-    },
-
-    //Every add method uses this.
-    //increasing the size in this.
-    _add_after: function (obj, prev_node) {
-        var node = this._new_node(obj);
-
-        if (prev_node === null) {
-            node.next = this._head;
-            this._head = node;
-        } else {
-            node.next = prev_node.next;
-            prev_node.next = node;
-            node.prev = prev_node;
-        }
-        
-        if (prev_node === this._tail) {
-            this._tail = node;
-        }
-        
+    if (this._size === 0) {
+        this._head = this._tail = node;
         ++this._size;
-        
         return node;
-    },
+    }
 
-    add: function (obj, index) {
-        index = index === undefined ? this._size : index;
-
-        if (index > this._size || index < 0) {
-            throw exceptions.OutOfBoundException;
-        }
-        
-        this._add_after(obj, this._get_node(index - 1));
-    },
-
-    addFirst: function (obj) {
-        this._add_after(obj, null);
-    }, 
-
-    addLast: function (obj) {
-        this.add(obj);
-    },
-    
-    addAll: function () {
-        this.addAllFromIndex(arguments, this._size - 1);
-    },
-    
-    addAllFromIndex: function (args, index) {
-        if (index > this._size || index < 0) {
-            throw exceptions.OutOfBoundException;
-        }
-
-        var prev_node = this._get_node(index - 1);
-        var i, len;
-        for (i = 0, len = args.length; i < len ; i++) {
-            var node = this._add_after(args[i], prev_node);
-            prev_node = node;
-        }
-    },
-
-    clear: function () {
-        this._head = null;
-        this._tail = null;
-        this._size = 0;
-    },
-    
-    indexOf: function (obj) {
-        var node = this._head;
-        var i = 0;
-        while (node !== null) {
-            if (node.obj === obj) {
-                return i;
-            }
-            ++i;
-        }
-        return -1;
-    },
-    
-    lastIndexOf: function (obj) {
-        var node = this._head;
-        var index = -1;
-        var i = 0;
-        while (node !== null) {
-            if (node.obj === obj) {
-                index = i;
-            }
-        }
-        return index;
-    },
-
-    remove: function (index) {
-        if (index > this._size || index < 0) {
-            throw exceptions.OutOfBoundException;
-        }
-        
-        //remove reference - let GC clean up
-        var node = this._get_node(index);
-        if (node.prev !== null) {
-            node.prev.next = node.next;
-        }
-        if (node.next !== null) {
-            node.next.prev = node.prev;
-        }
-        
-        --this._size;
-    },
-
-    removeFirst: function () {
-        if (this._size < 1) {
-            throw exception.NoSuchElementException;
-        }
-        this.remove(0);
-    },
-    
-    removeLast: function () {
-        if (this._size < 1) {
-            throw exception.NoSuchElementException;
-        }
-        this.remove(this._size - 1);
-    },
-    
-    size: function () {
-        return this._size;
+    if (index === 0) {
+        return this.insertBefore(node, this._head);
+    } else {
+        var prevNode = this.nodeAt(index - 1);
+        return this.insertAfter(node, prevNode);
     }
 };
 
-exports.LinkedList = LinkedList;
+LinkedList.prototype.insertAfter = function (obj, prevNode) {
+    var node = this._createNodeIfNot(obj);
+    assert(prevNode && prevNode.constructor === Node);
+    if (prevNode === this._tail) this._tail = node;
+    node._next = prevNode._next;
+    node._prev = prevNode;
+    prevNode._next = node;
+    if (node._next) node._next._prev = node;
+    ++this._size;
+    return node;
+};
+
+LinkedList.prototype.insertBefore = function (obj, nextNode) {
+    var node = this._createNodeIfNot(obj);
+    assert(nextNode && nextNode.constructor === Node);
+    if (nextNode === this._head) this._head = node;
+    node._next = nextNode;
+    node._prev = nextNode._prev;
+    nextNode._prev = node;
+    if (node._prev) node._prev._next = node;
+    ++this._size;
+    return node;
+};
+
+LinkedList.prototype.elementAt = function (index) {
+    var node = this.nodeAt(index);
+    return node ? node.data : undefined;
+};
+
+LinkedList.prototype.nodeAt = function (index) {
+    if (index >= this._size || index < 0) return undefined;
+    if (index < this._size/2) {
+        var node = this._head;
+        for (var i = 0 ; i < index ; i++) {
+            node = node._next;
+        }
+        return node;
+    } else {
+        var node = this._tail;
+        for (var i = 0, l = this._size - 1 - index; i < l ; i++) {
+            node = node._prev;
+        }
+        return node;
+    }
+};
+
+LinkedList.prototype.__defineGetter__("size", function () {
+    return this._size;
+});
+
+
+LinkedList.prototype.clear = function () {
+    this._head = null;
+    this._tail = null;
+    this._size = 0;
+};
+
+LinkedList.prototype.indexOf = function (obj) {
+    var node = this._head;
+    for (var i = 0, l = this._size; i < l && node; i++) {
+        if (node.data === obj) {
+            return i;
+        }
+        node = node._next;
+    }
+    return -1;
+};
+
+LinkedList.prototype.lastIndexOf = function (obj) {
+    var node = this._tail;
+    for (var i = 0, l = this._size; i < l && node; i++) {
+        if (node.data === obj) {
+            return this.size - 1 - i;
+        }
+    }
+    return -1;
+};
+
+LinkedList.prototype.empty = function () {
+    return this._size === 0;
+};
+
+LinkedList.prototype.remove = function (index) {
+    assert(index < this._size && index >= 0);
+    var node = this.nodeAt(index);
+    if (node !== this._head) {
+        node._prev._next = node._next;
+    } else {
+        this._head = node._next;
+        if (this._head._prev) this._head._prev = null;
+    }
+
+    if (node !== this._tail) {
+        node._next._prev = node._prev;
+    } else {
+        this._tail = node._prev;
+        if (this._tail._next) this._tail._next = null;
+    }
+
+    delete node._next && delete node._prev;
+    return --this._size;
+};
+
+// Private Methods
+LinkedList.prototype._createNodeIfNot = function (obj) {
+    return obj.constructor === Node ? obj : new Node(obj);
+};
