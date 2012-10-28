7651827a8a483fc6027eb26a16999635bd6f4c75
Satyam Shekhar <satyamshekhar@gmail.com>
2012-10-28 05:36:01 -0700

    Adding Stack.js

---
+++ src/Stack.js
@@ -0,0 +1,46 @@
+var Stack = module.exports = function () {
+    this._stack = [];
+};
+
+Stack.prototype.push = function () {
+    for (var i = 0, l = arguments.length; i < l; i++)
+        this._stack.push(arguments[i]);
+    return this._stack.length;
+};
+
+Stack.prototype.pop = function () {
+    return this._stack.pop();
+};
+
+Stack.prototype.empty = function () {
+    return this._stack.length === 0;
+};
+
+Stack.prototype.clear = function () {
+    delete this._stack;
+    this._stack = [];
+};
+
+Stack.prototype.foreach = function (Fn) {
+    for (var i = 0, l = this._stack.length; i < l; i++) {
+        Fn(this._stack[i]);
+    }
+};
+
+Stack.prototype.map = function (Fn) {
+    for (var i = 0, l = this._stack.length; i < l; i++) {
+        this._stack[i] = Fn(this._stack[i]);
+    }
+};
+
+Stack.prototype.toArray = function () {
+    return this._stack.slice();
+};
+
+Stack.prototype.__defineGetter__('size', function () {
+    return this._stack.length;
+});
+
+Stack.prototype.__defineGetter__('top', function () {
+    return this._stack[this._stack.length - 1];
+});
