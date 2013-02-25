433ecf4fd81ff33b659b775c60b2b9159c7bff30
Satyam Shekhar <satyamshekhar@gmail.com>
2013-02-25 01:30:55 -0800

    Add package.json and main file

---
+++ speedup.js
@@ -0,0 +1,6 @@
+exports.Stack = require("./src/Stack.js");
+exports.Queue = require("./src/Queue.js");
+exports.LinkedList = require("./src/LinkedList.js");
+exports.AVLTree = require("./src/AVLTree.js");
+exports.DisjointSet = require("./src/DisjointSet.js");
+exports.Heap = require("./src/Heap.js");
